0f6a2a240a1797a221213aa8e472386660f4b871
Mastore84 <mpb.petersen@gmail.com>
2022-09-30 08:52:13 +0200

    Added some extra commands and move descriptions

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -17,22 +17,34 @@ public class UserInterface {
                 System.out.println(game.getRoomDescription());
             }else if(userInput.equalsIgnoreCase("go south")){
                 game.go("south");
-                System.out.println("You enter the room.");
+                System.out.println("You enter the room to the south.");
             }else if(userInput.equalsIgnoreCase("go north")){
                 game.go("north");
-                System.out.println("You enter the room.");
+                System.out.println("You enter the room to the north.");
             }else if(userInput.equalsIgnoreCase("go east")){
                 game.go("east");
-                System.out.println("You enter the room.");
+                System.out.println("You enter the room to the east.");
             }else if(userInput.equalsIgnoreCase("go west")){
                 game.go("west");
-                System.out.println("You enter the room.");
+                System.out.println("You enter the room to the west.");
             }else if (userInput.equalsIgnoreCase("exit")){
                 System.out.println("Goodbye! Thanks for playing!");
                 shouldContinue = false;
+            }else if (userInput.equalsIgnoreCase("help")){
+                System.out.println("""
+                                    Available commands:
+                                    
+                                    'Go north': Move north of your position.
+                                    'Go south': Move south of your position.
+                                    'Go east': Move east of your position.
+                                    'Go west': Move west of your position.
+                                    'help': Display this help screen.
+                                    'exit': Exits the game.
+                                    
+                                    """);
             }
             else {
-                System.out.println("Invalid command. Please type 'go' followed by a direction");
+                System.out.println("Invalid command. Please type 'help' to get a list of available commands.");
             }
         }
     }
