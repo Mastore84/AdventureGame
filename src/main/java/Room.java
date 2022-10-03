c16fbf4ec9576b447f940efb7093bdce25e6b28d
Andsemandse <andr90m3@stud.kea.dk>
2022-10-03 13:32:57 +0200

    Påbegyndt metode for at tilføje Items(fortsætter iaften)

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -7,7 +7,7 @@ public class Room {
     private Room east;
     private Room west;
 
-    public Room(String roomName, String roomDescription){
+    public Room(String roomName, String roomDescription) {
         this.roomName = roomName;
         this.roomDescription = roomDescription;
         north = null;
@@ -16,66 +16,96 @@ public class Room {
         west = null;
     }
 
-    public void setNorth(Room room){
+    public void setNorth(Room room) {
         this.north = room;
     }
 
-    public void setSouth(Room room){
+    public void setSouth(Room room) {
         this.south = room;
     }
 
-    public void setEast(Room room){
+    public void setEast(Room room) {
         this.east = room;
     }
 
-    public void setWest(Room room){
+    public void setWest(Room room) {
         this.west = room;
     }
 
-    public Room getSouth(){
+    public Room getSouth() {
         return south;
     }
-    public Room getNorth(){
+
+    public Room getNorth() {
         return north;
     }
-    public Room getEast(){
+
+    public Room getEast() {
         return east;
     }
-    public Room getWest(){
+
+    public Room getWest() {
         return west;
     }
-    public String getRoomDescription(){
+
+    public String getRoomDescription() {
         return roomDescription;
     }
-    public Room getRoom(Room currentRoom){
+
+    public Room getRoom(Room currentRoom) {
         return currentRoom;
     }
-    public String getRoomName(){
+
+    public String getRoomName() {
         return roomName;
     }
-    public String getName(){
+
+    public String getName() {
         return roomName;
     }
+
     @Override
     public String toString() {
         String northName;
         String southName;
         String westName;
         String eastName;
-        if (north != null) northName = north.getName(); else northName = "null";
-        if (south != null) southName = south.getName(); else southName = "null";
-        if (west != null) westName = west.getName(); else westName = "null";
-        if (east != null) eastName = east.getName(); else eastName = "null";
+        if (north != null) northName = north.getName();
+        else northName = "null";
+        if (south != null) southName = south.getName();
+        else southName = "null";
+        if (west != null) westName = west.getName();
+        else westName = "null";
+        if (east != null) eastName = east.getName();
+        else eastName = "null";
         return "Room{" +
                 "roomName='" + getRoomName() + '\'' +
                 ", roomDescription='" + getRoomDescription() + '\'' +
-                ", north=" +  northName +
+                ", north=" + northName +
                 ", south=" + southName +
-                ", east=" +  eastName +
+                ", east=" + eastName +
                 ", west=" + westName +
                 '}';
     }
+
+
+//metode for at lave og tilføjer items
+
+    public void addItem(String description, String name) {
+        this.addItem(new Item(description, name));
+    }
+
+    public Item findItem(String itemName) {
+        for (Item item : items) {
+            if (item.getName().equalsIgnoreCase(itemName)) {
+                return item;
+
+            }
+        }
+        return null;
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
 }
-
-
-//Commit og push tester den lige baby
