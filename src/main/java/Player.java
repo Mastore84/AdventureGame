b493bd45c359cb138ee0af4aa121eeb18caa489a
Fluxi92 <loui019n@stud.kea.dk>
2022-10-06 14:36:12 +0200

    Update, lidt refaktorering

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -32,34 +32,7 @@ public class Player {
             }else return false;
         } return false;
     }
-    /*public Room go(String direction) {
-        currentRoom = currentRoom.getRoom(currentRoom);
-        if (direction.equalsIgnoreCase("south")) {
-            if (currentRoom.getSouth() != null) {
-                currentRoom = currentRoom.getSouth();
-                return currentRoom;
-            }else System.out.println("You cannot go in that direction");
-        }
-        else if (direction.equalsIgnoreCase("north")) {
-            if (currentRoom.getNorth() != null) {
-                currentRoom = currentRoom.getNorth();
-                return currentRoom;
-            }else System.out.println("You cannot go in that direction");
-        }
-        else if (direction.equalsIgnoreCase("east")) {
-            if (currentRoom.getEast() != null) {
-                currentRoom = currentRoom.getEast();
-                return currentRoom;
-            }else System.out.println("You cannot go in that direction");
-        }
-        else if (direction.equalsIgnoreCase("west")) {
-            if (currentRoom.getWest() != null) {
-                currentRoom = currentRoom.getWest();
-                return currentRoom;
-            }else System.out.println("You cannot go in that direction");
-        }return null;
-    }
-    */
+
     public String getRoomDescription(){
         return currentRoom.getRoomDescription();
     }
@@ -73,6 +46,3 @@ public class Player {
         this.currentRoom = currentRoom;
     }*/
 }
-
-
-
