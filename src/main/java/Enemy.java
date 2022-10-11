b9292533832ead4c351eae9f45420fef500fefe2
Mastore84 <mpb.petersen@gmail.com>
2022-10-11 13:48:21 +0200

    added Enemy class plus constructor and empty attack and getsHit methods

---
+++ src/main/java/Enemy.java
@@ -0,0 +1,21 @@
+public class Enemy {
+
+    private String name;
+    private String description;
+    private int healthPoints;
+    private Weapon weapon;
+
+    public Enemy(String name, String description, int healthPoints, Weapon weapon){
+        this.name = name;
+        this.description = description;
+        this.healthPoints = healthPoints;
+        this.weapon = weapon;
+    }
+
+    public void attack(){
+
+    }
+    public void getsHit(){
+
+    }
+}
